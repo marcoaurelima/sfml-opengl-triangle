1864089d3dcc96e2ab06d1bd129b462f9d8da890
Marco Aurélio Lima <marco_aurelio029@hotmail.com>
2022-08-20 23:14:18 -0300

    comentarios

--- main.cpp
+++ main.cpp
@@ -6,13 +6,20 @@
 
 int main(int argc, char **argv){
 
-    sf::Window window(sf::VideoMode(600,600), "teste");
+    // Para uso exclusivamente com OpenGL, basta `sf::Window`
+    // para uso de OpenGL juntamente com objetos SFML, usar `sf::RenderWindow`
+    // sf::Window window(sf::VideoMode(600,600), "teste");
+    sf::RenderWindow window(sf::VideoMode(600,600), "teste");
 
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     gluPerspective(45.0, window.getSize().x / window.getSize().y, 1.0, 500.0);
     glMatrixMode(GL_MODELVIEW);
+
+    sf::RectangleShape shape;
+    shape.setSize(sf::Vector2f(50,50));
+    shape.setFillColor(sf::Color::Red);
 
 
     bool running = true;
@@ -35,7 +42,12 @@ int main(int argc, char **argv){
             glVertex3f(2.0, -2.0, -5.0);
         }
         glEnd();
-        
+
+        // Se estiver usando `sf::RenderWindow`, estas duas funções delimitam
+        // onde chamar a função `draw` para desenhos do SFML.
+        window.pushGLStates();
+        window.draw(shape);
+        window.popGLStates();
 
         window.display();
     }
